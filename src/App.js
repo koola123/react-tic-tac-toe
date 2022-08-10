3c0ff5add2fe17ffd2095e774c2ff2b5dc0ca795
Snapper <snapper@Snappers-Air.fritz.box>
2022-08-11 00:40:30 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from './Board';
+import Square from './Square';
+import {useState, useEffect} from 'react';
+
+const fieldSquare = () => (new Array(9).fill(null));
+
+const lines = [
+  [0,1,2],  [3,4,5],  [6,7,8],  [0,3,6],
+  [1,4,7],  [2,5,8],  [0,4,8],  [2,4,6]
+];
+
 
 function App() {
+  const [squares, setSquares] = useState(fieldSquare);
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    const computerMove = squares.filter(square => square !== null).length % 2 === 1;
+    const linesThatAre = (a,b,c) => {
+      return lines.filter(squareIndexes => {
+        const squareValues = squareIndexes.map(index => squares[index]);
+        return JSON.stringify([a,b,c].sort()) === JSON.stringify(squareValues.sort());
+      });
+    };
+
+    const emptyIndexes = squares
+    .map((square, index) => square === null ? index : null)
+    .filter(val => val !== null);
+
+    const playerHasWon = linesThatAre ('x', 'x', 'x').length > 0;
+    const computerHasWon = linesThatAre('o', 'o', 'o').length > 0;
+      if (playerHasWon) {
+        setWinner('x');
+        alert('Player wins!');
+      }
+      if (computerHasWon) {
+        setWinner('o');
+        alert('Computer wins!')
+      }
+    const putComputerAt = index => {
+      let newSquares = squares;
+      newSquares[index] = 'o';
+      setSquares([...newSquares]);
+    };
+    if (computerMove) {
+
+      const linesToWin = linesThatAre('o', 'o', null);
+        if (linesToWin.length > 0) {
+        const winIndex = linesToWin[0].filter(index => squares[index] === null)[0];
+        putComputerAt(winIndex);
+        return;
+      }
+
+      const linesToBlock = linesThatAre('x', 'x', null);
+        if (linesToBlock.length > 0) {
+        const blockIndex = linesToBlock[0].filter(index => squares[index] === null)[0];
+        putComputerAt(blockIndex);
+        return;
+      }
+
+      const linesToContinueSmarter = linesThatAre('o', null, null);
+        if (linesToContinueSmarter.length > 0) {
+          putComputerAt(linesToContinueSmarter[0].filter(index => squares[index] === null)[0]);
+          return;
+        }
+
+      const randomIndex = emptyIndexes[Math.ceil(Math.random()*emptyIndexes.length)];
+        putComputerAt(randomIndex);
+      }
+    }, [squares]);
+
+  function handleSquareClick(index) {
+    const playerMove = squares.filter(square => square !== null).length % 2 === 0;
+    if (playerMove) {
+      let newSquares = squares;
+      newSquares[index] = 'x';
+      setSquares([...newSquares])
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='title'>Let's play - Tic, Tac, Toe</h1>
+      <div className='main'>
+        <Board>
+          {squares.map((square, index) =>
+            <Square
+            x = {square === 'x'? 1 : 0}
+            o = {square === 'o' ? 1 : 0}
+            onClick={() => handleSquareClick(index)}/>)}
+        </Board>
+        {!!winner && winner === 'x' && (
+          <div className="result green">
+            You WON!
+          </div>
+        )}
+        {!!winner && winner === '0' && (
+          <div className="result red">
+            You LOST!
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
